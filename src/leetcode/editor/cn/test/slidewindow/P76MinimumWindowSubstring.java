3eecc2dad60e4ec91e41e0d44fb7ecc5c0e0a385
shinysai <2352556786@qq.com>
2024-12-14 16:27:21 +0800

    [leetcode|slide window|2024.12.18] mac合入master P76最小覆盖子串

--- src/leetcode/editor/cn/test/slidewindow/P76MinimumWindowSubstring.java
+++ src/leetcode/editor/cn/test/slidewindow/P76MinimumWindowSubstring.java
@@ -24,20 +24,22 @@ public class P76MinimumWindowSubstring {
      * @return 最小子串
      */
     public String minWindow(String s, String t) {
-        // 如果s字符串长度小于t，那么肯定无法完全覆盖t字符串
         if (s.length() < t.length()) {
             return "";
         }
-        // 创建一个哈希表存储t的字符及字符数量
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
+        int slowIndex = 0, fastIndex = 0;
 
         return "";
 
     }
 
-
+    public boolean isContain(String s, char a) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == a) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
