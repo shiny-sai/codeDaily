a107409430f4528449704efc8e6ae8a501856f60
shinysai <2352556786@qq.com>
2024-12-18 23:00:05 +0800

    [leetcode|slide window|2024.12.16] 提交P76最小覆盖子串 题解

--- src/leetcode/editor/cn/test/slidewindow/P76MinimumWindowSubstring.java
+++ src/leetcode/editor/cn/test/slidewindow/P76MinimumWindowSubstring.java
@@ -11,7 +11,7 @@ public class P76MinimumWindowSubstring {
     // Test
     public static void main(String[] args) {
         P76MinimumWindowSubstring mws = new P76MinimumWindowSubstring();
-        String s = "abababbaa";
+        String s = "a";
         String t = "aa";
         String res = mws.minWindow(s, t);
         System.out.println(res);
@@ -24,22 +24,58 @@ public class P76MinimumWindowSubstring {
      * @return 最小子串
      */
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) {
-            return "";
+
+        // 创建两个哈希表 分别存储s和t的元素组成
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (char e : t.toCharArray()) {
+            tMap.put(e, tMap.getOrDefault(e, 0) + 1);
         }
+        // slowIndex 为滑动窗口左
         int slowIndex = 0, fastIndex = 0;
+        int len = s.length();
+        int ans = s.toCharArray().length + 1;
+        String res = "";
+        while (fastIndex < len) {
+            if (!check(sMap, tMap)) {
+                // 如果滑动窗口未包含t的所有字符，则fastIndex继续右移，使窗口中包含t的所有字符
+                if (tMap.containsKey(s.charAt(fastIndex))) {
+                    // 如果不加这个if判断会超时，因为不是每个字母都有必要关注的，只有t中的字符需要关注
+                    sMap.put(s.charAt(fastIndex), sMap.getOrDefault(s.charAt(fastIndex), 0) + 1);
+                }
+                fastIndex++;
+            }
+            while (check(sMap, tMap) && slowIndex <= fastIndex) {
+                // ans记录最小滑动窗口的长度
+                if (fastIndex - slowIndex < ans) {
+                    ans = fastIndex - slowIndex;
+                    res = s.substring(slowIndex, fastIndex);
+                }
+                // 如果滑动窗口中包含了t的所有字符，slowIndex向右移动查看包含t字符串的最小的滑动窗口
+                if (tMap.containsKey(s.charAt(slowIndex))) {
+                    sMap.put(s.charAt(slowIndex), sMap.get(s.charAt(slowIndex))-1);
+                }
+                slowIndex++;
+            }
 
-        return "";
-
+        }
+        return res;
     }
 
-    public boolean isContain(String s, char a) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == a) {
-                return true;
+    /**
+     * 比较两个哈希表的键值对，查看s中是否已经覆盖了t的所有字符，且数量也覆盖
+     * @param sMap s字符串对应的sMap
+     * @param tMap t字符串对于的tMap
+     * @return
+     */
+    public boolean check(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+        for (Character key : tMap.keySet()) {
+            if (tMap.get(key) > sMap.getOrDefault(key, 0)) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
 
